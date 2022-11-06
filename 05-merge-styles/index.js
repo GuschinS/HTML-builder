f6d8359df3e014ac2902039a027f0b5edf8649a3
guschins <guschins@gmail.com>
2022-11-06 14:52:01 +0300

    feat: add solution 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+
+const styles = path.join(__dirname, "styles");
+const projectDist = path.join(__dirname, "project-dist");
+
+fs.readdir(styles, { withFileTypes: true }, (err, files) => {
+  if (err) throw err;
+  let readStream;
+  const output = fs.createWriteStream(path.join(projectDist, "bundle.css"));
+  for (let file of files) {
+    if (file.name.split(".")[1] === "css") {
+      readStream = fs.createReadStream(path.join(styles, file.name), "utf-8");
+      readStream.pipe(output);
+    }
+  }
+});
