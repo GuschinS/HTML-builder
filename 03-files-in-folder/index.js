e892ad3d791badeb10193ab3b1a8d15cbf66afc2
guschins <guschins@gmail.com>
2022-11-07 15:37:50 +0300

    refactor: change throw err

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -7,11 +7,9 @@ fs.readdir(secretFolder, { withFileTypes: true }, (err, files) => {
   if (err) throw err;
   files.forEach((file) => {
     if (!file.isDirectory()) {
-      fs.stat(path.join(secretFolder, file.name), (error, stats) => {
-        if (error) {
-          console.log(error);
-        }
-        let extname = path.extname(file.name).slice(1);
+      fs.stat(path.join(secretFolder, file.name), (err, stats) => {
+        if (err) throw err;
+        let extname = file.name.split(".")[1];
         let fileName = file.name.split(".")[0];
         let fileSize = stats.size / 1000;
         console.log(`${fileName} - ${extname} - ${fileSize}kb`);
